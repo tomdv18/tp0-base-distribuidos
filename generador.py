c295dec5888ac573d1a461305db9674efa3f1889
tomdv18 <tdellavecchia@fi.uba.ar>
2025-03-17 19:37:20 +0000

    Agrego script y archivo python

---
+++ generador.py
@@ -0,0 +1,58 @@
+import sys
+import yaml
+
+def generar_docker_compose(archivo_salida, cantidad_clientes):
+    docker_compose = {
+        "name": "tp0-Tom",
+        "services": {
+            "server": {
+                "container_name": "server",
+                "image": "server:latest",
+                "entrypoint": "python3 /main.py",
+                "environment": [
+                    "PYTHONUNBUFFERED=1",
+                    "LOGGING_LEVEL=DEBUG"
+                ],
+                "networks": ["testing_net"]
+            }
+        },
+        "networks": {
+            "testing_net": {
+                "ipam": {
+                    "driver": "default",
+                    "config": [{"subnet": "172.25.125.0/24"}]
+                }
+            }
+        }
+    }
+
+    for i in range(1, cantidad_clientes + 1):
+        nombre = f"client{i}"
+        docker_compose["services"][nombre] = {
+            "container_name": nombre,
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [
+                f"CLI_ID={i}",
+                "CLI_LOG_LEVEL=DEBUG"
+            ],
+            "networks": ["testing_net"],
+            "depends_on": ["server"]
+        }
+
+    with open(archivo_salida, 'w') as file:
+        yaml.dump(docker_compose, file, default_flow_style=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Error: Se deben proporcionar dos parámetros: el nombre del archivo y la cantidad de clientes.")
+        sys.exit(1)
+
+    archivo_salida = sys.argv[1]
+    try:
+        cantidad_clientes = int(sys.argv[2])
+    except ValueError:
+        print("Error: La cantidad de clientes debe ser un número entero válido.")
+        sys.exit(1)
+
+    generar_docker_compose(archivo_salida, cantidad_clientes)
---
+++ generar-compose.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Valores por defecto
+ARCHIVO="docker-compose-dev-tom.yaml"
+CLIENTES=3
+
+if [ -z "$1" ]; then
+  echo "No se proporcionó nombre de archivo, usando valor por defecto: $ARCHIVO"
+  archivo_salida=$ARCHIVO
+else
+  archivo_salida=$1
+fi
+
+if [ -z "$2" ]; then
+  echo "No se proporcionó cantidad de clientes, usando valor por defecto: $CLIENTES"
+  cantidad_clientes=$CLIENTES
+
+else
+  if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+    echo "Error: La cantidad de clientes debe ser un número entero positivo."
+    exit 1
+  fi
+  cantidad_clientes=$2
+fi
+
+
+echo "Nombre del archivo de salida: $archivo_salida"
+echo "Cantidad de clientes: $cantidad_clientes"
+
+
+python3 generador.py $archivo_salida $cantidad_clientes
