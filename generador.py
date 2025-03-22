057cbd816e4492b7b22dc492fe794d9b36ceaed1
tomdv18 <tdellavecchia@fi.uba.ar>
2025-03-22 11:43:19 -0300

    fix generador.py

--- generador.py
+++ generador.py
@@ -23,8 +23,8 @@ def generar_docker_compose(archivo_salida, cantidad_clientes):
                 "image": "alpine:latest",
                 "networks": ["testing_net"],
                 "volumes":[
-                "- ./validar_echo_server.sh:/validar_echo_server.sh",
-                "- ./server/config.ini:/config.ini"
+                "./validar_echo_server.sh:/validar_echo_server.sh",
+                "./server/config.ini:/config.ini"
                  ],
                 "depends_on": ["server"]
             }
