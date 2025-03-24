fe326b734f841a58f21699d6fd9f728c86237c34
tomdv18 <tdellavecchia@fi.uba.ar>
2025-03-24 14:59:07 -0300

    Fix Generador

--- generador.py
+++ generador.py
@@ -44,7 +44,7 @@ def generar_docker_compose(archivo_salida, cantidad_clientes):
             ],
             "networks": ["testing_net"],
             "volumes": [
-                "./client/config.yaml:/config.yaml"
+                "./client/config.yaml:/config.yaml",
                 f"./.data/agency-{i}.csv:/agency.csv"
             ],
             "depends_on": ["server"]
